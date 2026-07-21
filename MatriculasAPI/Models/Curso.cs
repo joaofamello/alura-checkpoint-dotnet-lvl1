c605711c5d477475e72cff342e695c73eaac2fb6
João Francisco <joaofrancisco.amello@gmail.com>
2026-07-21 12:42:30 -0300

    feat: adição de um identificador na classe curso

--- MatriculasAPI/Models/Curso.cs
+++ MatriculasAPI/Models/Curso.cs
@@ -2,6 +2,7 @@
 
 public class Curso
 {
+    public int Id { get; set; }
     public string Titulo { get; set; }
     public string Descricao { get; set; }
     public string Categoria { get; set; }
