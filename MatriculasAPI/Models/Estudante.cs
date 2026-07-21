1685225e50d9df46ece93a182cfef32e754ef325
João Francisco <joaofrancisco.amello@gmail.com>
2026-07-21 12:39:33 -0300

    feat: criação do modelo Estudante

---
+++ MatriculasAPI/Models/Estudante.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MatriculasAPI.Models;
+
+public class Estudante : IdentityUser
+{
+    public string NomeCompleto { get; set; }
+    public DateOnly DataCadastro { get; set; }
+}
