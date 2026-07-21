42d18e51dcff55b2c0a0a5c9f94a654c792580b8
João Francisco <joaofrancisco.amello@gmail.com>
2026-07-21 12:35:08 -0300

    feat: criação do model Curso

---
+++ MatriculasAPI/Models/Curso.cs
@@ -0,0 +1,10 @@
+﻿namespace MatriculasAPI.Models;
+
+public class Curso
+{
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public string Categoria { get; set; }
+    public int CargaHoraria { get; set; }
+    public DateOnly DataCadastro { get; set; }
+}
